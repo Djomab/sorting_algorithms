7457917cb95506650b79bb93a4654e497cb083a9
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-17 17:25:42 +0100

    adding not null condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
+	if (array == NULL || size == 0)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = i + 1; j < size; j++)
