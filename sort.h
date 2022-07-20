d454977ac40095b9ef1b52017c2d149144c41662
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-20 08:15:15 +0100

    Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -10,9 +10,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /** PROTOTYPES **/
@@ -23,5 +23,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-
 #endif
